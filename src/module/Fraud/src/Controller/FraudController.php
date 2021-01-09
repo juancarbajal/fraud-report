fc966e6c768210e36f1d2c31e36a29e1bbb9cde0
Juan Carbajal <jcarbajal@MacJuan.local>
2021-01-08 20:22:07 -0500

    con card

--- src/module/Fraud/src/Controller/FraudController.php
+++ src/module/Fraud/src/Controller/FraudController.php
@@ -9,16 +9,16 @@ class FraudController extends AbstractActionController
 {
     private function getDatabase(){
         //$link = \mssql_connect('sugoServer', 'admin', 'A8WYS9q2*z');
-	$serverName = "prod-sugo-apps.cjefewagaayr.us-east-1.rds.amazonaws.com";
-	$connectionOptions = array(
-	    "Database" => "db-sugo-vtext-01",
-	    "Uid" => "admin",
-	    "PWD" => "A8WYS9q2*z",
-	    "Port" => "1433"
-	);
-		    
-	$link = sqlsrv_connect($serverName,$connectionOptions);
-        return $link;
+        $serverName = "prod-sugo-apps.cjefewagaayr.us-east-1.rds.amazonaws.com";
+        $connectionOptions = array(
+            "Database" => "db-sugo-vtext-01",
+            "Uid" => "admin",
+            "PWD" => "A8WYS9q2*z",
+            'CharacterSet' => 'UTF-8'
+        );
+                
+        $link = sqlsrv_connect($serverName,$connectionOptions);
+            return $link;
 
     }
     public function indexAction()
@@ -28,9 +28,36 @@ class FraudController extends AbstractActionController
 
     public function creditCardAction()
     {
-        $db = $this->getDatabase();
-
-        return new ViewModel();
+        $from = $_REQUEST['desde'];
+        $to = $_REQUEST['hasta'];
+        if (isset($from) && isset($to)){
+            $db = $this->getDatabase();
+            $sql = "select 
+            paymentsystemname
+            , cardfirstdigits
+            , lastdigits
+            , count(1)
+            from 
+            (
+                select 
+                distinct(email)
+                , paymentsystemname
+                , cardfirstdigits
+                , lastdigits
+                from ordenes
+                where creationdate BETWEEN '$from' and '$to'
+            ) as m 
+            group by paymentsystemname
+            , cardfirstdigits
+            , lastdigits
+            having count(1)>1;";
+            $stmt = sqlsrv_query( $db, $sql);
+            if( $stmt === false ) {
+                die( print_r( sqlsrv_errors(), true));
+            }
+            $data = sqlsrv_fetch_object( $stmt);
+        }
+        return new ViewModel(['data' => $data]);
     }
 
     public function documentAction()
