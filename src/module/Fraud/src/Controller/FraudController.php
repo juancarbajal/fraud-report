79aa4dca673077a68629a12185fd296d8c10f75c
Juan Carbajal <jcarbajal@MacJuan.local>
2021-01-09 00:00:38 -0500

    con card

--- src/module/Fraud/src/Controller/FraudController.php
+++ src/module/Fraud/src/Controller/FraudController.php
@@ -55,7 +55,7 @@ class FraudController extends AbstractActionController
             paymentsystemname , 
             cardfirstdigits , 
             lastdigits 
-            
+            into ##tmpCreditCard
             from ordenes 
             where creationdate BETWEEN '2020-12-01' and '2020-12-30' ;
             ";
@@ -71,10 +71,8 @@ class FraudController extends AbstractActionController
             group by paymentsystemname
             , cardfirstdigits
             , lastdigits
-            having count(1)>1;
             ";
-            $sql2 = "select top 10 * from ordenes;";
-$sql1 = $sql2;
+            
             $sql3 = "drop table ##tmpCreditCard;";
 
             $db = $this->getDatabase();
